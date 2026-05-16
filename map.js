2a45fbfaf86cd59181ad4a8e0e789c7ecfcb08a0
Haowen Zhang <haz099@ucsd.edu>
2026-05-15 17:23:15 -0700

    .

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuMTI0MyIsImEiOiJjbXA2NW1veWkxZGp6MnFvaXhtNGJueDN2In0.89HZphX7NnNWu-YbuwAthg';
@@ -53,5 +54,13 @@ map.on('load', () => {
       'line-opacity': 0.6
     }
   });
+
+  const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+
+    const jsonData = await d3.json(jsonurl);
+    console.log('Loaded JSON Data:', jsonData);
+
+    let stations = jsonData.data.stations;
+    console.log('Stations Array:', stations);
 
 });
