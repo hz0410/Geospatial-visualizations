9e541d80723ca365324c83377b737f09aeb0ca7c
Haowen Zhang <haz099@ucsd.edu>
2026-05-15 18:05:02 -0700

    ..

--- map.js
+++ map.js
@@ -17,10 +17,11 @@ const map = new mapboxgl.Map({
 });
 
 
+
 function getCoords(station) {
-  const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
-  const { x, y } = map.project(point);
-  return { cx: x, cy: y };
+  const point = new mapboxgl.LngLat(+station.lon, +station.lat); // Convert lon/lat to Mapbox LngLat
+  const { x, y } = map.project(point); // Project to pixel coordinates
+  return { cx: x, cy: y }; // Return as object for use in SVG attributes
 }
 
 map.on('load', async () => {
